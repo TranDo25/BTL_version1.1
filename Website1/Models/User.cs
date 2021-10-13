a4c31ff52c1b0fbfe61c7001acaa1e8d08006daf
Đặng Vinh <ndqvinh1411@gmail.com>
2021-10-13 22:32:23 +0700

    day len tu may vinh

---
+++ Website1/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website1.Models
+{
+    public class User
+    {
+        public String Username { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+    }
+}
